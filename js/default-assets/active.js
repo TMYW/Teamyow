79717d2677e4b44c393601d86cf598aa5955a8ca
Alex Balate <alexjbalate02@gmail.com>
2021-01-07 01:51:02 +0800

    Major Changes
    Adding Clothing Page & other Changes.

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -229,5 +229,99 @@
 //   });
 
 
+    // *********************************
+    // :: Magnific Popup Active Code - clothing
+    // *********************************
+    if ($.fn.magnificPopup) {
+        $('.clothing-img').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true,
+                preload: [0, 2],
+                navigateByImgClick: true,
+                tPrev: 'Previous',
+                tNext: 'Next'
+            }
+        });
+    }
+
+     // *********************************
+    // :: 5.0 Masonary Gallery Active Code - clothing
+    // *********************************
+
+    if ($.fn.imagesLoaded) {
+        $('.alime-clothing').imagesLoaded(function () {
+            // filter items on button click
+            $('.clothing-menu').on('click', 'button', function () {
+                var filterValue = $(this).attr('data-filter');
+                $grid.isotope({
+                    filter: filterValue
+                });
+            });
+            // init Isotope
+            var $grid = $('.alime-clothing').isotope({
+                itemSelector: '.single_gallery_item',
+                percentPosition: true,
+                masonry: {
+                    columnWidth: '.single_gallery_item'
+                }
+            });
+        });
+    }
+
+    // ***********************************
+    // :: 6.0 Portfolio Button Active Code
+    // ***********************************
+    
+    $('.clothing-menu button.btn').on('click', function () {
+        $('.clothing-menu button.btn').removeClass('active');
+        $(this).addClass('active');
+    })
+
+
+
+      // *********************************
+    // :: 3.0 Welcome Slides Active Code
+    // *********************************
+
+    if ($.fn.owlCarousel) {
+        var welcomeSlider = $('.Clothing-slides');
+        welcomeSlider.owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            smartSpeed: 1000,
+            autoplayTimeout: 10000,
+            nav: true,
+            navText: [('<i class="ti-arrow-left"></i>'), ('<i class="ti-arrow-right"></i>')]
+        })
+
+        welcomeSlider.on('translate.owl.carousel', function () {
+            var layer = $("[data-animation]");
+            layer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).removeClass('animated ' + anim_name).css('opacity', '0');
+            });
+        });
+
+        $("[data-delay]").each(function () {
+            var anim_del = $(this).data('delay');
+            $(this).css('animation-delay', anim_del);
+        });
+
+        $("[data-duration]").each(function () {
+            var anim_dur = $(this).data('duration');
+            $(this).css('animation-duration', anim_dur);
+        });
+
+        welcomeSlider.on('translated.owl.carousel', function () {
+            var layer = welcomeSlider.find('.owl-item.active').find("[data-animation]");
+            layer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).addClass('animated ' + anim_name).css('opacity', '1');
+            });
+        });
+    }
+
 })(jQuery);
 
